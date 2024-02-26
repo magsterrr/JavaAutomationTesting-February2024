e5bcc828af5975aabb1783436d0eebc19f2a2d37
Magdalena Trifonova <112561246+magsterrr@users.noreply.github.com>
2024-02-26 22:14:51 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide your first name: ");
+
+        String firstName = scanner.nextLine();
+
+        System.out.println("Hello "+firstName);
+
+    }
+}
