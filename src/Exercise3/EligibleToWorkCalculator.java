acd6f80357f23cdc365077f44e31f7525d20cee5
Magdalena Trifonova <112561246+magsterrr@users.noreply.github.com>
2024-03-04 21:06:18 +0200

    Create EligibleToWorkCalculator - third exercise

---
+++ src/Exercise3/EligibleToWorkCalculator.java
@@ -0,0 +1,21 @@
+package Exercise3;
+
+import java.util.Scanner;
+
+public class EligibleToWorkCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int inputNumber = Integer.parseInt((scanner.nextLine()));
+
+        if (inputNumber >= 16) {
+            System.out.println("Congrats! You are eligible to work.");
+        }
+        else {
+            System.out.println("Sorry! You are not eligible to work");
+        }
+    }
+}
+
